bc6e8375acf29665793573e2686f094205f9b74c
yahir2021 <namucheyahir@gmail.com>
2022-03-02 17:48:29 -0500

    python

---
+++ impar o par.py
@@ -0,0 +1,21 @@
+numero = int(input("Escriba un número del 1 al 10: "))
+if numero == 1:
+    print("¡Ha escrito un numero impar!")
+if numero == 2:
+    print("¡Ha escrito un numero par!")
+if numero == 3:
+    print("¡Ha escrito un numero impar!")
+if numero == 4:
+    print("¡Ha escrito un numero par!")
+if numero == 5:
+    print("¡Ha escrito un numero impar!")
+if numero == 8:
+    print("¡Ha escrito un numero par!")
+if numero == 7:
+    print("¡Ha escrito un numero impar!")
+if numero == 8:
+    print("¡Ha escrito un numero par!")
+if numero == 9 :
+    print("¡Ha escrito un numero impar!")
+if numero == 10:
+    print("¡Ha escrito un numero par!")
